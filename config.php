79d7789e8a46e2cda8868e4982c35bf4a9b81962
Sebastiano <160122164+Sebahhhhh@users.noreply.github.com>
2026-05-05 11:01:59 +0200

    Add files via upload

--- config.php
+++ config.php
@@ -4,7 +4,7 @@ $dbname = 'verifica_rocchi';
 $user   = 'root';
 $password = '';
 
-// classico PDO 
+// classico PDO
 try {
     $pdo = new PDO("mysql:host=$host;dbname=$dbname;charset=utf8", $user, $password);
     $pdo->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
